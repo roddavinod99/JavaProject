2b4099b5ac23fd35c0bd561e592c9df79aa52346
Vinod Kumar Rodda <76143573+roddavinod99@users.noreply.github.com>
2022-03-02 15:09:41 +0530

    Initial commit

---
+++ src/com/Internshala/javaapp/Main.java
@@ -0,0 +1,16 @@
+package com.Internshala.javaapp;
+
+import jdk.management.resource.internal.inst.SocketOutputStreamRMHooks;
+
+import java.sql.SQLOutput;
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+       // incrememnt operator
+        int year = 10;
+        System.out.println(year++ - ++ year + --year);
+
+    }
+}
