a9f0bc6871b111efd4235d2823718fc6272dff3c
Vinod Kumar Rodda <76143573+roddavinod99@users.noreply.github.com>
2022-03-03 01:14:44 +0530

    code challenge

--- src/com/Internshala/javaapp/Main.java
+++ src/com/Internshala/javaapp/Main.java
@@ -4,55 +4,18 @@ import com.sun.xml.internal.ws.api.streaming.XMLStreamWriterFactory;
 
 import java.util.Scanner;
 
-public class Main {
+public class Main
+{
 
     public static void main(String[] args) {
-       // IF-ELSE IF  ladder statement
-        char grade;
-        int marks = 85;
-
-        if (marks >= 90)
-        {
-            grade='A';
+        boolean green = true;
+        if (green) {
+            System.out.println("You are allowed to move");
+        } else {
+            System.out.println("Please stop");
         }
-        else if (marks >= 80)
-        {
-            grade = 'B';
-        }
-        else if (marks >= 70)
-        {
-            grade = 'C';
-        }
-        else if (marks >= 60)
-        {
-            grade = 'D';
-        }
-        else
-        {
-            grade = 'F';
-        }
-        System.out.println(" Grade :" + grade);
-
-        switch (grade)
-        {
-            case 'A':
-                 System.out.println("Excellent marks");
-            break;
-            case 'B':
-                System.out.println(" Very good marks");
-            break;
-            case 'C':
-                System.out.println("Good marks, Could do better");
-            break;
-            case 'D':
-                System.out.println(" Average score, Work hard");
-            break;
-            case 'F':
-                System.out.println("You have failed");
-            break;
-            default :
-                System.out.println("Invalid Grade");
-        }
-
+        String message;
+        message = (green) ? " You are allowed to move" : "Please stop";
+        System.out.println(message);
     }
 }
