64cf2b37ed68026840861af97b3c59d33a854dd7
Vinod Kumar Rodda <76143573+roddavinod99@users.noreply.github.com>
2022-03-23 18:28:03 +0530

    class and methods- Core Java Cource

---
+++ src/com/Internshala/javaapp/Dog.java
@@ -0,0 +1,15 @@
+package com.Internshala.javaapp;
+
+public class Dog {
+
+	public String breed;
+	public int size;
+	public String colour;
+
+	public void bark(){
+		System.out.println("wooh! wooh!");
+	}
+	public void run(){
+		System.out.println("I am a "+breed+". I am running.");
+	}
+}
--- src/com/Internshala/javaapp/Main.java
+++ src/com/Internshala/javaapp/Main.java
@@ -5,56 +5,29 @@ public class Main
 {
     public static void main(String[] args)
     {
-        System.out.println(" TAX CALCULATION APP ");
-        System.out.println(" ------welcome------ ");
+        Dog dog1= new Dog();
+        dog1.breed = "Labrador";
+        dog1.size = 50 ;
+        dog1.colour= "Black";
 
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("Enter the number of people: ");
-        int n = scanner.nextInt();            // scans  the value and assigns to create the size of the arrays
+        dog1.bark();
+        dog1.run();
 
-        String[] name= new String[n]; // takes name
-        long[] income = new long[n];  // takes income
+        System.out.println(dog1.breed);
+        System.out.println(dog1.size);
+        System.out.println(dog1.colour);
 
+        Dog dog2= new Dog();
+        dog2.breed= "Dollmation";
+        dog2.size= 20;
+        dog2.colour= "Black white";
 
-        for (int i=0;i< name.length; i++ )    //rely on assignment solution to know what was wrong
-        {
-            System.out.println("Enter the names " + (i+1)+ " :");
-            name[i]= scanner.next();
-            System.out.println(" Enter the " + name[i] + "'s Income: ");
-            income[i]= scanner.nextLong();
+        dog2.bark();
+        dog2.run();
 
-        }
-        scanner.close();
-
-        System.out.println("Tax to be payed:");
-        System.out.println("----------------");
-
-
-
-        for (int i=0; i< n ; i++)
-        {
-            calculateTax(name[i], income[i]); // method declared below
-        }
-
-    }
-    public static void calculateTax(String name, long income)
-    {
-        long tax;  // since income is in long
-
-        if (income>=300000)  // if income is more than 1lakhs
-        {
-            tax = income*20 / 100;
-        } else if (income>= 100000 && income<= 300000)  //if income is more than 1 lakhs and less than 3 lakhs
-        {
-            tax= income * 10/100;
-        } else  // if income is less than 1 lakhs
-        {
-            tax = 0;
-        }
-
-        System.out.println(" " + name + "'s tax is : " + '\u20B9' + " " + tax); // print the income to be payed by the individual
-
-
+        System.out.println(dog2.breed);
+        System.out.println(dog2.size);
+        System.out.println(dog2.colour);
     }
 
 }
