cebb73361c8311f3291f41d4a212c96df37d10a1
Vinod Kumar Rodda <76143573+roddavinod99@users.noreply.github.com>
2022-04-08 13:41:24 +0530

    new project

--- src/com/Internshala/javaapp/Main.java
+++ src/com/Internshala/javaapp/Main.java
@@ -1,38 +1,10 @@
 package com.Internshala.javaapp;
-import java.util.Scanner;
 
 public class Main
 {
-    public static void main(String[] args)
-    {
-        Dog dog1= new Dog();
-        dog1.breed = "Labrador";
-        dog1.size = 50 ;
-        dog1.colour= "Black";
+	public static void main(String[] args)
+	{
 
-        dog1.bark();
-        dog1.run();
-
-        System.out.println(dog1.breed);
-        System.out.println(dog1.size);
-        System.out.println(dog1.colour);
-
-        Dog dog2= new Dog();
-        dog2.breed= "Dollmation";
-        dog2.size= 20;
-        dog2.colour= "Black white";
-
-        dog2.bark();
-        dog2.run();
-
-        System.out.println(dog2.breed);
-        System.out.println(dog2.size);
-        System.out.println(dog2.colour);
-    }
-
+	}
 }
-
-
-
-
 
